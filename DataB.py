48e89f7b7c0b3f240631f16be9b9b21094e60365
aryabtsev <aryabtsev@users.noreply.github.com>
2017-05-21 21:51:58 +0300

    update

--- DataB.py
+++ DataB.py
@@ -1,22 +1,52 @@
-import sqlite3
+import pymysql
+import datetime
 
 class DataB:
 
-    def __init__(self, database):
-        self.connection = sqlite3.connect(database)
+    def __init__(self):
+        self.connection = pymysql.connect(host = "173.230.151.37", port = 3306,  user = "root", passwd = "BMT4Ever", database = "mydb")
         self.cursor = self.connection.cursor()
 
 
     def select_average_hr(self):
         # получаем среднее сердцебиение
         with self.connection:
-            return self.cursor.execute('SELECT AVG(Heart_rate) FROM TeleMedicine').fetchall()
+            return self.cursor.execute('SELECT AVG(Heart_rate) FROM Telemedicine')
 
 
     def select_average_br(self):
         # получаем среднее дыхание
         with self.connection:
-            return self.cursor.execute('SELECT AVG(Breath_rate) FROM TeleMedicine').fetchall()
+            return self.cursor.execute('SELECT AVG(Breath_rate) FROM Telemedicine')
+
+    def select_average_br(self):
+        # получаем среднее дыхание
+        with self.connection:
+            return self.cursor.execute('SELECT AVG(Breath_rate) FROM Telemedicine')
+
+    def select_day_hr(self):
+         # получаем чсс за день
+        date_input = datetime.date.isoformat(datetime.date.today())
+        query = str('SELECT Heart_rate FROM Telemedicine WHERE date == %s' % date_input)
+
+        with self.connection:
+            return self.cursor.execute(query)
+
+    def select_day_br(self):
+        # получаем чд за день
+        date_input = datetime.date.isoformat(datetime.date.today())
+        query = str('SELECT Breath_rate FROM Telemedicine WHERE date == %s' % date_input)
+
+        with self.connection:
+            return self.cursor.execute(query)
+
+
+
+
+                    #SELECT * FROM
+     #   your_table_name
+     #   WHERE
+      #  your_field_name = "whatever";
 
     def close(self):
         # Закрываем текущее соединение с БД
